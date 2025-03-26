4edd6f07b8dad717ae33af145101368753ba0b47
abe.hu <abe.hu@docusign.com>
2025-03-26 10:43:40 -0700

    add rest of shell playwright tests

--- src/__tests__/test-utils/envs.ts
+++ src/__tests__/test-utils/envs.ts
@@ -1,4 +1,4 @@
-// import { Page, environment } from '@1ds/qe';
+import { Page } from '@playwright/test';
 
 // import { HostedEnvironment } from '@1ds/helpers/types';
 // import { WidgetConfig } from '../../isomorphic/types/widgetConfigs.types';
@@ -28,34 +28,34 @@
 //   }, configName);
 // };
 
-// /**
-//  * Extracts widget config from the DOM using the widget.frame div that surrounds the mounted widget
-//  * @param page
-//  * @param widgetId
-//  * @returns
-//  */
-// export const getWidgetConfigFromPage = async (
-//   page: Page,
-//   widgetId: string,
-// ): Promise<WidgetConfig> => {
-//   return await page.evaluate(async (widgetId: string) => {
-//     const widgetConfigs = document?.querySelector(
-//       `script[type="application/json"][data-1ds-config-id="widget-config"]`,
-//     );
+/**
+ * Extracts widget config from the DOM using the widget.frame div that surrounds the mounted widget
+ * @param page
+ * @param widgetId
+ * @returns
+ */
+export const getWidgetConfigFromPage = async (
+  page: Page,
+  widgetId: string,
+): Promise<any> => {
+  return await page.evaluate(async (widgetId: string) => {
+    const widgetConfigs = document?.querySelector(
+      `script[type="application/json"][data-1fe-config-id="widget-config"]`,
+    );
 
-//     try {
-//       const widgetConfig = JSON.parse(widgetConfigs?.innerHTML || '[]');
-//       const widgetConfigForId = widgetConfig.find(
-//         (config: WidgetConfig) => config.widgetId === widgetId,
-//       );
-//       return widgetConfigForId;
-//     } catch (error) {
-//       throw new Error(
-//         `Could not parse widget-config from the DOM for widgetId: ${widgetId}`,
-//       );
-//     }
-//   }, widgetId);
-// };
+    try {
+      const widgetConfig = JSON.parse(widgetConfigs?.innerHTML || '[]');
+      const widgetConfigForId = widgetConfig.find(
+        (config: any) => config.widgetId === widgetId,
+      );
+      return widgetConfigForId;
+    } catch (error) {
+      throw new Error(
+        `Could not parse widget-config from the DOM for widgetId: ${widgetId}`,
+      );
+    }
+  }, widgetId);
+};
 
 // export const isProdEnvironmentRun = ['production', 'demo'].includes(
 //   process.env.NODE_ENV?.toLowerCase() || 'production',

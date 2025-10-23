546afda7eb55eb1b9b4d1371b6b779b1db2e7926
Luke Hatcher <lukehatcher98@gmail.com>
2025-10-23 09:54:21 -0700

    fix: csp (#36)

--- src/csp-configs.ts
+++ src/csp-configs.ts
@@ -13,7 +13,8 @@ export const enforcedDefaultCsp: Record<string, CSPPerEnvironment> = {
   },
   integration: {
     ...commonCsp,
-    scriptSrc: [...commonCsp.scriptSrc, "'unsafe-eval'"], // required for the playground to pass widget props
+    scriptSrc: [...commonCsp.scriptSrc, "'unsafe-eval'", "http://127.0.0.1:*", "http://localhost:*"], // required for the playground to pass widget props
+    connectSrc: [...commonCsp.connectSrc, "http://127.0.0.1:*", "http://localhost:*"],
   },
   production: commonCsp,
 };
